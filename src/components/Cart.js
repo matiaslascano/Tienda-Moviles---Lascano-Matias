fd2ef17dfe2fbef689ec2ae081c9e5877b1c5525
matiaslascano <matias_lascano@hotmail.com>
2022-09-19 10:13:53 -0300

    Se realiza lo solicitado en el desafio 11 -  Crea tu colección de órdenes

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,9 +3,31 @@ import { Link } from "react-router-dom";
 import { useCartContext } from "../CartContext";
 import ItemCart from "./ItemCart";
 import "./Cart.css";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
 
 const Cart = () => {
   const { cart, totalPrecio, clearCart } = useCartContext();
+
+  const order = {
+    comprador: {
+      nombre: "Matias",
+      email: "matias_lascano@hotmail.com",
+      telefono: "+5493514084026",
+      direccion: "Edmundo de Amicis 4010",
+    },
+    items: cart.map((product) => ({
+      id: product.id,
+      nombre: product.nombre,
+      precio: product.precio,
+      cantidad: product.cantidad,
+    })),
+    total: totalPrecio(),
+  };
+  const handleClick = () => {
+    const db = getFirestore();
+    const ordersCollection = collection(db, "orders");
+    addDoc(ordersCollection, order).then(({ id }) => console.log(id));
+  };
 
   if (cart.length === 0) {
     return (
@@ -29,6 +51,7 @@ const Cart = () => {
         Vaciar Carrito
       </button>
       <p className="precioTotal">Total: ${totalPrecio()}</p>
+      <button onClick={handleClick}>Emitir Orden de Compra</button>
     </>
   );
 };
